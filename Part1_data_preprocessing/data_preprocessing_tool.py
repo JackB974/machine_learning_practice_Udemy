244d147a9548daf1b20833fdec5566c0169dd267
Aurelien <aurelien.banabera@epitech.eu>
2025-11-05 14:06:31 +0100

    commit

--- Part1_data_preprocessing/data_preprocessing_tool.py
+++ Part1_data_preprocessing/data_preprocessing_tool.py
@@ -6,6 +6,8 @@ import numpy as np
 import pandas as pd
 #matplotlib : plot charts/graph
 import matplotlib.pyplot as plt
+#scikitlearn
+from sklearn.impute import SimpleImputer
 
 
 # Importing the dataset
@@ -17,6 +19,23 @@ dataset = pd.read_csv("Data.csv")
 x = dataset.iloc[:, :-1].values #takes every values except alst colum
 #take alst (dependable variabe = thing we are trying to predict)
 y = dataset.iloc[:, -1].values
-print(y)
+#print(y)
 
+#Handle missing data
+#replacing missing data by average of all salary for example
+#create object of simpleimputer class
+#np.nan ==> numpy lib for missing values
+# ==> replace all missing value by the mean (strategy = replace missing value by...)
+#SimpleImputer(missingvalues, replace by)
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+#now precise on which colum/row to apply replacement ==> fit (var on wich to applu // []==> the iloc used on x)
+#Tip: include all numerical columns values
+imputer.fit(x[:, 1:3])
+#do the deed! w/ transform
+#trasnform ==> return colulns with replacement
+#so here says replace x by the transformed imputer of X
+x[:, 1:3] = imputer.transform(x[:, 1:3])
+
+#do'nt print umputer print the X, the columns/tableau we did the transform and such on
+print(x)
 
